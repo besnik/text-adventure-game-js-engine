7b36ed7adefea60033ad1c132dc9a39a17b16d9b
Slavo Hudak <slavomir.hudak@sk.ibm.com>
2016-05-18 23:12:18 +0200

    Added parser from JSON to Locations object model.

--- poc/engine/engine1/engine.js
+++ poc/engine/engine1/engine.js
@@ -16,12 +16,13 @@ var Location = function() {
    this.links = [];             // array of links (locations) where player can move from this location
 }
 
-var Locations = function(items) {
-    for (var i in items) {
-        var item = items[i];
-        this[item.id] = item;
+var Locations = function(locations) {
+    for (var i in locations) {
+        var l = locations[i];
+        this[l.id] = l;
     }
 }
+
 
 var Game = function(player, locations) {
     this.player = player;
@@ -34,5 +35,39 @@ var Game = function(player, locations) {
     this.location = function() {
         return this.locations[this.player.locationId];    
     }
+}
+
+
+var Parser = function() {
+    
+    // returns array of Location type from given json input
+    this.toLocations = function(json) {
+    
+        var locations = [];
+    
+        for (var i in json) {
+            var locationJson = json[i];
+            
+            // create model from input level definition json
+            var l = new Location();
+            
+            l.id = locationJson.id;
+            l.text = locationJson.text;
+            l.title = locationJson.title;
+            
+            l.links = [];
+            for (var i in locationJson.links) {
+                var linkJson = locationJson.links[i];
+                l.links.push(new Link(linkJson.id, linkJson.text));
+            }
+            
+            // set as property of locations object        
+            locations.push(l);
+        }   
+        
+        return locations;
+    
+    }
+    
 }
 
