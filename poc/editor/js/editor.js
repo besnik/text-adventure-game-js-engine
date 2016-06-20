a9f916e9619116a55b1bb285feb73294d765b3a6
Slavo Hudak <slavomir.hudak@sk.ibm.com>
2016-06-20 14:58:19 +0200

    Implemented Level Editor dialog.

--- poc/editor/js/editor.js
+++ poc/editor/js/editor.js
@@ -11,17 +11,19 @@ String.prototype.supplant = function (o) {
 };
 
 // logs error message. error is always logged. if not error then it depends on global settings in editor.debug
-function log(message, isError) {
+function log(message, isError, ex) {
     if (typeof message != "string") return;
     if (typeof isError != "boolean") isError = false;
 
     if (editor.debug || isError) { 
         try {
             prefix = isError ? "ERROR: " : "INFO: ";
+            if (typeof ex != "undefined" && ex.stack) { message += ex.stack }
+
             console.log(prefix + message);
         } 
         catch(ex) {
-            // your browser is not supporting console.log
+            // your browser does not support console.log
         }
     }
 }
@@ -30,14 +32,35 @@ function log(message, isError) {
 // MODEL ///////////////////////////////////////////////////////////////////////////////////
 
 var editor = {
-    debug: true
-};
-
-
-editor.game = {
-    id: "New game id",
-    levels : []
+    debug: true,
+    game: {
+        id: "New game id"
+    }
 }
+
+editor.game.levels = {
+    items: [],
+    get: function(id) {
+        result = this.items.find(function(level){ return level.id == id });
+        if (result == undefined) { result = null; }
+        return result;
+    },
+    add: function(level) {
+        return this.items.push(level);
+    }
+}
+
+editor.factory = {
+    createLevel: function(id, name, text) {
+        return {
+            id: id,
+            name: name,
+            text: text
+        };
+    }
+}
+
+
 
 
 // VIEW ////////////////////////////////////////////////////////////////////////////////////
@@ -55,7 +78,7 @@ editor.ui.hideElement = function (el) {
 }
 
 editor.ui.showElement = function (el) {
-    log("Showing element " + el);
+    log("Showing element \"" + el.id + "\" HTML: " + el.outerHTML.substr(0, 100));
     editor.ui.setCssDisplay(el, "block");
 }
 
@@ -78,22 +101,67 @@ editor.ui.levelEditor.errorMsg = editor.ui.levelEditor.el.querySelector("#errorM
 
 editor.ui.levelEditor.btnBack.addEventListener("click", function() {
     log("Event: click, Element: Back button of Level Editor");
-    editor.ui.gameEditor.show();
+    try {
+        editor.ui.gameEditor.show();
+    }
+    catch(ex) {
+        var errorMsg = "Error in Back button event handler of Level editor. Details: " + ex.message;
+        log(errorMsg, true, ex);
+        editor.ui.levelEditor.showErrorMsg(errorMsg);
+    }
 });
 
 editor.ui.levelEditor.btnSave.addEventListener("click", function() {
     log("Event: click, Element: Save button of Level Editor");
-    // TODO: validation
-    // TODO: update model (this triggers event that in turn updates view)
-    
-    editor.ui.levelEditor.reset();
-    editor.ui.gameEditor.show();
+    try {
+        // Validation
+        if (!editor.ui.levelEditor.isValid()) { return; }
+
+        // Get values from input html elements
+        var levelId = editor.ui.levelEditor.el.querySelector("#txtLevelId").value;
+        var levelName = editor.ui.levelEditor.el.querySelector("#txtLevelName").value;
+        var levelText = editor.ui.levelEditor.el.querySelector("textarea").value
+        
+        // Create and store model
+        var level = editor.factory.createLevel(levelId, levelName, levelText);
+        editor.game.levels.add(level);
+        
+        // Clear form for next usage and show game editor
+        editor.ui.levelEditor.reset();
+        editor.ui.gameEditor.show();
+    }
+    catch(ex) {
+        var errorMsg = "Error in Save button event handler of Level editor. Details: " + ex.message;
+        log(errorMsg, true, ex);
+        editor.ui.levelEditor.showErrorMsg(errorMsg);
+    }
 });
 
-editor.ui.levelEditor.validate = function() {
-    // TODO: are all inputs non-empty? call view
-    // TODO: is level id unique? call model
-    // TODO: show error message if needed
+editor.ui.levelEditor.isValid = function() {
+    
+    // 1. TEXT BOXES NON EMPTY
+    var errorMsg = "All text boxes must be non-empty."
+    var inputs = editor.ui.levelEditor.el.querySelectorAll("input");
+    for (var i = 0; i < inputs.length; i++) { 
+        if (inputs[i].value == "") {
+            editor.ui.levelEditor.showErrorMsg(errorMsg);
+            return false;
+        }
+    }
+
+    if (editor.ui.levelEditor.el.querySelector("textarea").value == "") {
+        editor.ui.levelEditor.showErrorMsg(errorMsg);
+        return false;
+    }  
+    
+    // 2. LEVEL ID IS UNIQUE
+    var levelId = editor.ui.levelEditor.el.querySelector("#txtLevelId").value;
+    if (editor.game.levels.get(levelId) != null) {
+        editor.ui.levelEditor.showErrorMsg("Specified level ID is already used.");
+        return false;
+    }
+
+    return true;
 }
 
 editor.ui.levelEditor.show = function() { 
